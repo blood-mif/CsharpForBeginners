ce0f6043afdf28385b637e555dbd07711830c690
blood-mif <78865084+blood-mif@users.noreply.github.com>
2021-02-12 17:34:00 +0300

    1
    Com

---
+++ BlackJack/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+
+namespace Mentor_2 //21 очко
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string[] desk = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "V", "D", "K", "T", "2", "3", "4", "5", "6", "7", "8", "9", "10", "V", "D", "K", "T", "2", "3", "4", "5", "6", "7", "8", "9", "10", "V", "D", "K", "T", "2", "3", "4", "5", "6", "7", "8", "9", "10", "V", "D", "K", "T" };
+           
+            Random random = new Random();
+
+            
+            int cartValueUser = 0;
+            int yourPoints =0;
+            int compPoints =0;
+
+            for (int yourHand = 0; yourHand < 22;)
+            {
+                string cartValue = desk[random.Next(0, 52)];
+
+
+                Console.WriteLine("У вас карта " + cartValue);
+                if (cartValue == "V" || cartValue == "D" || cartValue == "K")
+                {
+                    cartValueUser  = 10;
+                    yourHand = cartValueUser + yourHand;
+                    if (yourHand > 22)
+                    {
+                        Console.WriteLine("Вы проиграли, у вас перебор " + yourHand);
+                        break;
+                    }
+                }
+                
+                else if (cartValue =="T")
+                {
+                    if (yourHand <= 10)
+                    {
+                        cartValueUser = 11;
+                    }
+                    else
+                    {
+                        cartValueUser = 1;
+                    }
+                    yourHand = cartValueUser + yourHand;
+                    if (yourHand > 22)
+                    {
+                        Console.WriteLine("Вы проиграли, у вас перебор " + yourHand);
+                        break;
+                    }
+                }
+                else
+                {
+                    cartValueUser = int.Parse(cartValue);
+                    yourHand = cartValueUser + yourHand;
+                    if (yourHand > 22)
+                    {
+                        Console.WriteLine("Вы проиграли, у вас перебор " + yourHand);
+                        break;
+                    }
+                }
+                Console.WriteLine("У вас очков = " + yourHand);
+
+                Console.WriteLine("Взять еще карту? (y/n)");
+                string again = Console.ReadLine();
+                if (again == "y")
+                {
+                        continue;
+                }
+                else
+                    {
+                        Console.WriteLine("у вас " + yourHand);
+                    yourPoints = yourHand;
+                    break;
+                    
+                    }
+                
+            }
+            Console.WriteLine("А теперь ходит комп");
+            // Бытые нарушено, random в цикле карждый раз генерится заново, что позволяет появление повторов =(
+            for (int compHand = 0; compHand < 22; compHand++)
+            {
+                string cartValue = desk[random.Next(0, 52)];
+
+                if (cartValue == "V" || cartValue == "D" || cartValue == "K")
+                {
+                    cartValueUser = 10;
+                    compHand = cartValueUser + compHand;
+                    if (compHand > 22)
+                    {
+                        Console.WriteLine("Комп проиграл, у него перебор " + compHand);
+                        break;
+                    }
+                }
+
+                else if (cartValue == "T")
+                {
+                    if (compHand <= 10)
+                    {
+                        cartValueUser = 11;
+                    }
+                    else
+                    {
+                        cartValueUser = 1;
+                    }
+                    compHand = cartValueUser + compHand;
+                    if (compHand > 22)
+                    {
+                        Console.WriteLine("Комп проиграл, у него перебор " + compHand);
+                        break;
+                    }
+                }
+                else
+                {
+                    cartValueUser = int.Parse(cartValue);
+                    compHand = cartValueUser + compHand;
+                    if (compHand > 22)
+                    {
+                        Console.WriteLine("Комп проиграл, у него перебор " + compHand);
+                        break;
+                    }
+                }
+                compPoints = compHand;
+                Console.WriteLine("У компа карта "+cartValue +" в сумме очков "+ compPoints);
+            }
+            if (yourPoints >= compPoints && yourPoints < 22)
+            {
+                Console.WriteLine("Вы победили");
+            }
+            if (yourPoints < compPoints && compPoints < 22)
+            {
+                Console.WriteLine("Вы проиграли");
+            }
+
+        }
+    }
+}
---
+++ Ментор_2_3_1/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace Калькулятор
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            double firstValue;
+            double secondValue;
+            bool result;
+
+            static void addition(double firstValue, double secondValue) 
+            {
+                double res = firstValue + secondValue;
+                Console.WriteLine("Сложение чисел " + firstValue + " и " + secondValue + " = " + res);
+            }
+
+            static void difference(double firstValue, double secondValue)
+            {
+                double res = firstValue - secondValue;
+                Console.WriteLine("Вычитание чисел " + firstValue + " и " + secondValue + " = " + res);
+            }
+
+            static void multiplication(double firstValue, double secondValue)
+            {
+                double res = firstValue * secondValue;
+                Console.WriteLine("Умножение чисел " + firstValue + " и " + secondValue + " = " + res);
+            }
+
+            static void division(double firstValue, double secondValue)
+            {
+                if (secondValue == 0)
+                {
+                    Console.WriteLine("На ноль делить нельзя!!!");
+                    Console.ReadLine();
+                    
+                }
+                double res = firstValue / secondValue;
+                Console.WriteLine("Деление чисел чисел = " + firstValue + " и " + secondValue + " = " + res);
+            }
+
+            while (true)
+            {
+
+                Console.WriteLine("Введите первое число: ");
+                str = Console.ReadLine();
+                result = double.TryParse(str, out firstValue);
+                if (result)
+                {
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка ввода второго числа, используйте только числа");
+                    Console.ReadLine();
+                    continue;
+                }
+
+                Console.WriteLine("Введите второе число: ");
+                str = Console.ReadLine();
+                result = double.TryParse(str, out secondValue);
+                if (result)
+                {
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка ввода второго числа, используйте только числа");
+                    Console.ReadLine();
+                    continue;
+                }
+
+                Console.WriteLine("Введите желаемую операцию: + - * / ");
+                str = Console.ReadLine();
+                if (str == "+")
+                {
+                    addition(firstValue, secondValue);
+                }
+
+                else if (str == "-")
+                {
+                    difference(firstValue, secondValue);
+                }
+
+
+                else if (str == "*")
+                {
+                    multiplication(firstValue, secondValue);
+                }
+
+                else if (str == "/")
+                {
+                    division(firstValue, secondValue);
+                }
+                else
+                {
+                    Console.WriteLine("Вы ввели не верный символ математической операции");
+                }
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
