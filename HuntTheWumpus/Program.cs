1bf4ecaa4e31bf217cc2b081d4f327da4e522ef1
blood-mif <78865084+blood-mif@users.noreply.github.com>
2021-02-19 19:40:06 +0300

    1

---
+++ HuntTheWumpus/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+
+namespace HuntTheWumpus
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Game game = new Game();
+            game.Start();
+        }
+    }
+}
